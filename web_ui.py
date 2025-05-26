c403a003773fbe1f8b940a74a1378a6f0dfd77ed
Soham Roy <sohamroy52@gmail.com>
2025-05-26 22:11:49 +0530

    Initial Commit

--- web_ui.py
+++ web_ui.py
@@ -1,9 +1,16 @@
 import streamlit as st
+from streamlit import checkbox
 
+import functions
 
+todos =functions.get_todos()
 st.title("My Todos App")
+st.write("This App is designed to increase your productivity")
 
-def add_todos():
-    pass
+st.checkbox("test")
+# def add_todos():
+#     pass
 
+for todo in todos:
+    st.checkbox(todo)
 
